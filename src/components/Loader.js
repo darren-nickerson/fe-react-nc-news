c3b70c1aceda1f7ed3d2256c3dc4a8f640718b42
Darren Nickerson <45294843+darren-nickerson@users.noreply.github.com>
2023-07-19 16:58:57 +0100

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,10 +3,12 @@ import '../components/Loader.css';
 
 const Loading = () => {
   return (
+    <div>
+            <p> Please be informed that our API currently operates using the free version of Render. While we strive to provide you with a seamless experience, please note that the database might take some time to spin up. We apologize for any inconvenience this may cause and appreciate your patience. </p>
+
     <div className='spinnerContainer'>
       <FaSpinner className='spins' />
-        <p> Please be informed that our API currently operates using the free version of Render. While we strive to provide you with a seamless experience, please note that the database might take some time to spin up. We apologize for any inconvenience this may cause and appreciate your patience. </p>
-
+    </div>
     </div>
     
   );
